e2761862e467aba990e5fb41a3b2c374dec381be
Steffen Gebert <steffen.gebert@typo3.org>
2013-12-06 19:16:27 +0100

    Import cookbook skeleton

---
+++ spec/default_spec.rb
@@ -0,0 +1,10 @@
+require "spec_helper"
+
+# Write unit tests with ChefSpec - https://github.com/sethvargo/chefspec#readme
+describe "skeleton::default" do
+  let (:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
+
+  it "does something" do
+    pending "Replace this with meaningful tests"
+  end
+end
