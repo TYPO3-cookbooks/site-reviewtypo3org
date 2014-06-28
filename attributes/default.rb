dc7d339b253e96d66637d49e77f85d98eeb56a24
Steffen Gebert <steffen.gebert@typo3.org>
2014-06-28 21:01:11 +0200

    Add data base name

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,8 @@
 default['git']['hostname'] = "dev.git.typo3.org"
 default['git']['hostname'] = "git.typo3.org" if node.chef_environment == "production"
 
-
-default['gerrit']['config']['database']['type'] = "mysql"
+default['gerrit']['config']['database']['type'] = "MYSQL"
+default['gerrit']['config']['database']['database'] = "gerrit"
 default['gerrit']['config']['auth']['type'] = "HTTP"
 default['gerrit']['config']['auth']['cookieSecure'] = true
 default['gerrit']['config']['auth']['gitBasicAuth'] = true
