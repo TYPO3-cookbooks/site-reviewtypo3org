2e9509a458a088045c78dae025a8b9efbcd7fad3
Steffen Gebert <steffen.gebert@typo3.org>
2014-06-28 16:06:53 +0200

    Add attributes from t3-gerrit/attributes/default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,10 +1,20 @@
-# generic defaults
+default['git']['hostname'] = "dev.git.typo3.org"
+default['git']['hostname'] = "git.typo3.org" if node.chef_environment == "production"
 
+
+default['gerrit']['config']['database']['type'] = "mysql"
+default['gerrit']['config']['auth']['type'] = "HTTP"
+default['gerrit']['config']['auth']['cookieSecure'] = true
+default['gerrit']['config']['auth']['gitBasicAuth'] = true
+default['gerrit']['proxy']['ssl']['enable'] = true
+
+
+# AMPQ worker
 default['site-reviewtypo3org']['amqp']['server'] = nil
 default['site-reviewtypo3org']['amqp']['user'] = nil
 default['site-reviewtypo3org']['amqp']['vhost'] = nil
-
 default['site-reviewtypo3org']['mq-worker']['gerrit']['user'] = 'gerrit-mq-worker'
+
 
 # override defaults for production
 if node.chef_environment == 'production'
