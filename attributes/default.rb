8ee3cac29cc1cd0f79a51bce6b87c0bb077b2c21
Steffen Gebert <steffen.gebert@typo3.org>
2014-06-28 16:10:21 +0200

    Extract AMQP attributes to extra file

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,27 +7,3 @@ default['gerrit']['config']['auth']['type'] = "HTTP"
 default['gerrit']['config']['auth']['cookieSecure'] = true
 default['gerrit']['config']['auth']['gitBasicAuth'] = true
 default['gerrit']['proxy']['ssl']['enable'] = true
-
-
-# AMPQ worker
-default['site-reviewtypo3org']['amqp']['server'] = nil
-default['site-reviewtypo3org']['amqp']['user'] = nil
-default['site-reviewtypo3org']['amqp']['vhost'] = nil
-default['site-reviewtypo3org']['mq-worker']['gerrit']['user'] = 'gerrit-mq-worker'
-
-
-# override defaults for production
-if node.chef_environment == 'production'
-  default['site-reviewtypo3org']['amqp'] = {
-    'server' => 'mq.typo3.org',
-    'user' => 'reviewtypo3org',
-    'vhost' => 'infrastructure'
-  }
-# override defaults for pre-production
-elsif node.chef_environment == 'pre-production'
-  default['site-reviewtypo3org']['amqp'] = {
-    'server' => 'mq.typo3.org',
-    'user' => 'devreviewtypo3org',
-    'vhost' => 'infrastructure_dev'
-  }
-end
