4d3f3bccfa23db264cb74471a2353acb4e3133fe
Steffen Gebert <steffen.gebert@typo3.org>
2014-06-28 16:08:25 +0200

    Add attributes from t3-gerrit/attributes/theme.rb

---
+++ attributes/theme.rb
@@ -0,0 +1,12 @@
+override['gerrit']['theme']['compile_files'] = %w{
+  GerritSiteHeader.html
+  GerritSite.css
+}
+
+override['gerrit']['theme']['static_files'] = %w{
+  401-redirect-to-wiki.html
+  footer-logo.b9f2ba66aab906975d481c42d4.cache.gif
+  s-buttons.2a9e3abb93e617254323a2f81b.cache.png
+  typo3-logo.3512c0c5a9ad8c03b900fc7531e08.cache.png
+  typo3.css
+}
