410fc226e3ffb335cfee399ee9bbdc42b5dae3d2
Steffen Gebert <steffen.gebert@typo3.org>
2014-07-24 00:00:41 +0200

    Seems there was already a 0.1.4 somewhere..

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Steffen Gebert / TYPO3 Association"
 maintainer_email "steffen.gebert@typo3.org"
 license          "Apache 2.0"
 description      "Installs/configures something"
-version          "0.1.4"
+version          "0.1.5"
 
 depends "ssh"
 depends "ssl_certificates"
