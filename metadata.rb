28246093e6ebe33ca6284c53594befb7c2cc4543
Steffen Gebert <steffen.gebert@typo3.org>
2014-07-24 00:10:47 +0200

    Version 0.1.9

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Steffen Gebert / TYPO3 Association"
 maintainer_email "steffen.gebert@typo3.org"
 license          "Apache 2.0"
 description      "Installs/configures something"
-version          "0.1.8"
+version          "0.1.9"
 
 depends "ssh"
 depends "ssl_certificates"
