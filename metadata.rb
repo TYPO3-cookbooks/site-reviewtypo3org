c75f43efdfb3640abd21f14f5f44431ff5892af3
Steffen Gebert <steffen.gebert@typo3.org>
2014-11-22 21:56:47 +0100

    Bump version to 0.1.11

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Steffen Gebert / TYPO3 Association"
 maintainer_email "steffen.gebert@typo3.org"
 license          "Apache 2.0"
 description      "Installs/configures something"
-version          "0.1.10"
+version          "0.1.11"
 
 depends "ssh"
 depends "ssl_certificates"
