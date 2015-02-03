c5f1f37191e33d3f9cc78f490d4d1c2333c4569d
Steffen Gebert <steffen.gebert@typo3.org>
2015-02-03 14:36:47 +0100

    Read password from chef-vault

--- recipes/amqp-publisher.rb
+++ recipes/amqp-publisher.rb
@@ -4,6 +4,11 @@ remote_file "#{node['gerrit']['install_dir']}/plugins/rabbitmq.jar" do
   group node['gerrit']['group']
 end
 
+# read AMQP password from chef-vault
+include_recipe "t3-chef-vault"
+amqp_pass = chef_vault_password(node['site-reviewtypo3org']['amqp']['server'], node['site-reviewtypo3org']['amqp']['user'])
+
+
 template "#{node['gerrit']['install_dir']}/etc/rabbitmq.config" do
   source "amqp-publisher/rabbitmq.config.erb"
   owner node['gerrit']['user']
@@ -12,7 +17,7 @@ template "#{node['gerrit']['install_dir']}/etc/rabbitmq.config" do
     :amqp => {
       :uri => "amqp://#{node['site-reviewtypo3org']['amqp']['server']}/#{node['site-reviewtypo3org']['amqp']['vhost']}",
       :username => node['site-reviewtypo3org']['amqp']['user'],
-      :password => "test"
+      :password => amqp_pass
     }
   )
 end
