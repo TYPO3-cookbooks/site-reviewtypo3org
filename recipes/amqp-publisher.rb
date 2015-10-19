d1d2a69e07fa17a21b0ebbff9778782506544b7d
Steffen Gebert <steffen.gebert@typo3.org>
2015-10-19 23:17:53 +0200

    Use temporary URL for rabbitmq plugin download

--- recipes/amqp-publisher.rb
+++ recipes/amqp-publisher.rb
@@ -1,5 +1,7 @@
 remote_file "#{node['gerrit']['install_dir']}/plugins/rabbitmq.jar" do
-  source "https://github.com/TYPO3-infrastructure/gerrit-rabbitmq-plugin/releases/download/rabbitmq-1.5-SNAPSHOT-20150203154700/rabbitmq-1.5-SNAPSHOT-20150203154700.jar"
+  source "http://st-g.de/fileadmin/downloads/2015-10/rabbitmq-1.5-SNAPSHOT-20150203154700.jar"
+  # this should be the following URL, but somehow this doesn't work today (see http://stackoverflow.com/questions/33223904/chef-remote-file-download-from-github-fails)
+  # source "https://github.com/TYPO3-infrastructure/gerrit-rabbitmq-plugin/releases/download/rabbitmq-1.5-SNAPSHOT-20150203154700/rabbitmq-1.5-SNAPSHOT-20150203154700.jar"
   owner node['gerrit']['user']
   group node['gerrit']['group']
 end
