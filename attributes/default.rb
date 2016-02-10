2f0290ddc5518c7194cf332f7618c7210da9104f
Steffen Gebert <steffen.gebert@typo3.org>
2016-02-10 23:55:12 +0100

    Move production attributes from role to here

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,14 +1,16 @@
 default['gerrit']['version'] = "2.12"
 
-default['git']['hostname'] = "dev.git.typo3.org"
-default['git']['hostname'] = "git.typo3.org" if node.chef_environment == "production"
+default['gerrit']['hostname'] = "review.typo3.org"
 
 default['gerrit']['config']['gerrit']['canonicalWebUrl'] = "https://#{node['gerrit']['hostname']}/"
+default['gerrit']['config']['gerrit']['canonicalGitUrl'] = "git://#{node['gerrit']['hostname']}"
 default['gerrit']['config']['database']['type'] = "MYSQL"
 default['gerrit']['config']['database']['database'] = "gerrit"
 default['gerrit']['config']['auth']['type'] = "HTTP"
 default['gerrit']['config']['auth']['cookieSecure'] = true
 default['gerrit']['config']['auth']['gitBasicAuth'] = true
-default['gerrit']['proxy']['ssl'] = true
-
 default['gerrit']['batch_admin_user']['enabled'] = true
+default['gerrit']['proxy']['ssl'] = true
+default['gerrit']['proxy']['ssl_cabundle'] = "/etc/ssl_certs/wildcard.typo3.org.ca-bundle"
+default['gerrit']['proxy']['ssl_certfile'] = "/etc/ssl_certs/wildcard.typo3.org.crt"
+default['gerrit']['proxy']['ssl_keyfile'] = "/etc/ssl_certs/wildcard.typo3.org.key"
