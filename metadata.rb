4f6cf02af260f6a6106d7983810074c5fa29a167
Steffen Gebert <st+github@st-g.de>
2016-08-28 16:15:39 +0200

    [TASK] Give depsolver few hints

--- metadata.rb
+++ metadata.rb
@@ -5,13 +5,13 @@ license          "Apache 2.0"
 description      "Installs/configures something"
 version          "0.2.4"
 
-depends "t3-base",           "~> 0.2.0"
+depends "t3-base",           "~> 0.2.51"
 
-depends "site-gittypo3org",  "~> 0.1.0"
+depends "site-gittypo3org",  "~> 0.1.19"
 depends "ssl_certificates",  "~> 1.1.0"
 depends "t3-gerrit",         "~> 0.5.5"
 depends "t3-chef-vault",     "~> 1.0.0"
 depends "t3-mysql",          "~> 0.1.3"
 
 depends "ssh",               "= 0.6.6"
-depends "build-essential"
+depends "build-essential" ,  "= 6.0.4"
