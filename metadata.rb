bd9805f514f9da6d42613991e5a995a418a9c531
Steffen Gebert <steffen.gebert@typo3.org>
2016-10-02 17:05:46 +0200

    [TASK] Update dependencies

--- metadata.rb
+++ metadata.rb
@@ -9,9 +9,11 @@ depends "t3-base",           "~> 0.2.51"
 
 depends "site-gittypo3org",  "~> 0.1.19"
 depends "ssl_certificates",  "~> 1.1.0"
-depends "t3-gerrit",         "~> 0.5.5"
+depends "gerrit",            "~> 0.6.0"
+depends "t3-gerrit",         "~> 0.6.0"
 depends "t3-chef-vault",     "~> 1.0.0"
 depends "t3-mysql",          "~> 0.1.3"
 
+depends "php",               "= 1.1.2"
 depends "ssh",               "= 0.6.6"
 depends "build-essential" ,  "= 6.0.4"
