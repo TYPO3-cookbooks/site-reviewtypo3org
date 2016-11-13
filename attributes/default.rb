84fccd4641a21a6337f7ad3b868531b0ecb1224a
Steffen Gebert <steffen.gebert@typo3.org>
2016-11-13 09:56:15 +0100

    [TASK] Set proxy-https with override

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,8 +4,8 @@ default['gerrit']['hostname'] = "review.typo3.org"
 
 default['gerrit']['config']['gerrit']['canonicalWebUrl'] = "https://#{node['gerrit']['hostname']}/"
 default['gerrit']['config']['gerrit']['canonicalGitUrl'] = "git://#{node['gerrit']['hostname']}"
-#<> Signal to Gerrit that our proxy speaks HTTPS
-default['gerrit']['config']['httpd']['listenUrl'] = "proxy-https://127.0.0.1:8080"
+#<> Signal to Gerrit that our proxy speaks HTTPS. As gerrit::proxy sets this with 'normal' precedence, we have to 'override' here.
+override['gerrit']['config']['httpd']['listenUrl'] = "proxy-https://127.0.0.1:8080"
 
 default['gerrit']['config']['database']['type'] = "MYSQL"
 default['gerrit']['config']['database']['database'] = "gerrit"
