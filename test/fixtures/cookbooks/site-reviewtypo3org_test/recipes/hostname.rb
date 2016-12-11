705956cffc0e747cb89699a750e180eeb12a48cc
Steffen Gebert <steffen.gebert@typo3.org>
2016-12-11 09:26:57 +0100

    [TESTS] Hostsfile cookbook 2.4.5 does not play well in Docker
    We need to wait until customink-webops/hostsfile#78 is released.

--- test/fixtures/cookbooks/site-reviewtypo3org_test/recipes/hostname.rb
+++ test/fixtures/cookbooks/site-reviewtypo3org_test/recipes/hostname.rb
@@ -1,4 +1,18 @@
-hostsfile_entry node['ipaddress'] do
-  hostname 'review.vagrant'
-  aliases ['git.vagrant']
+# does not work inside docker, until this is released:
+# https://github.com/customink-webops/hostsfile/pull/78
+# Current version 2.4.5 doesn't contain this.
+
+#hostsfile_entry node['ipaddress'] do
+#  hostname 'review.vagrant'
+#  aliases ['git.vagrant']
+#end
+
+ruby_block "update /etc/hosts" do
+  block do
+    fe = Chef::Util::FileEdit.new("/etc/hosts")
+    fe.insert_line_if_no_match(/#{node['ipaddress']}/,
+                               "#{node['ipaddress']} review.vagrant git.vagrant")
+    fe.write_file
+  end
+  not_if { Resolv.getaddress('review.vagrant') rescue false }
 end
