434097db7c74f7cd576b61e9611b759b38046a96
Steffen Gebert <steffen.gebert@typo3.org>
2017-10-22 20:51:24 +0200

    [TESTS] Alternate ini syntax for inspec

--- test/integration/default/inspec/gerrit_spec.rb
+++ test/integration/default/inspec/gerrit_spec.rb
@@ -14,14 +14,14 @@ control 'gerrit-1' do
 
   describe ini('/var/gerrit/review/etc/gerrit.config') do
     its(['gerrit', 'reportBugUrl']) { should cmp 'http://forge.typo3.org/projects/team-git' }
-    its('database.type') { should cmp 'MYSQL' }
-    its('auth.gitBasicAuth') { should cmp 'true' }
-    its('gc.interval') { should cmp '3 days' }
-    its('gc.startTime') { should cmp '3:00' }
-    its('httpd.listenUrl') { should cmp 'http://0.0.0.0:8080/' }
-    its('download.scheme') { should cmp 'anon_http' } # we can only check for the last occurrence wit the ini resource
-    its('sendemail.from') { should cmp 'Gerrit Code Review <gerrit_dontreply@typo3.org>' }
-    its('sendemail.includeDiff') { should cmp 'true' }
+    its(['database', 'type']) { should cmp 'MYSQL' }
+    its(['auth', 'gitBasicAuth']) { should cmp 'true' }
+    its(['gc', 'interval']) { should cmp '3 days' }
+    its(['gc', 'startTime']) { should cmp '3:00' }
+    its(['httpd', 'listenUrl']) { should cmp 'http://0.0.0.0:8080/' }
+    its(['download', 'scheme']) { should cmp 'anon_http' } # we can only check for the last occurrence wit the ini resource
+    its(['sendemail', 'from']) { should cmp 'Gerrit Code Review <gerrit_dontreply@typo3.org>' }
+    its(['sendemail', 'includeDiff']) { should cmp 'true' }
   end
 
   [8080, 29418].each do |listen_port|
