ba41347c98b5ec7f98135b0549b711683952b47d
Steffen Gebert <steffen.gebert@typo3.org>
2017-12-01 17:40:19 +0100

    [TESTS] Add proxy suite

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,8 +6,8 @@ default['gerrit']['hostname'] = "review.typo3.org"
 default['gerrit']['config']['gerrit']['canonicalWebUrl'] = "https://#{node['gerrit']['hostname']}/"
 #<> Gerrit's Git URL (used in emails etc)
 default['gerrit']['config']['gerrit']['canonicalGitUrl'] = "git://#{node['gerrit']['hostname']}"
-#<> Signal to Gerrit that our proxy speaks HTTPS. As gerrit::proxy sets this with 'normal' precedence, we have to 'override' here. Using ::1 instead of 127.0.0.1 prevents confusing inspec.
-override['gerrit']['config']['httpd']['listenUrl'] = 'http://0.0.0.0:8080/'
+#<> Signal to Gerrit that our proxy speaks HTTPS.
+default['gerrit']['config']['httpd']['listenUrl'] = 'http://0.0.0.0:8080/'
 #<> Database type
 default['gerrit']['config']['database']['type'] = "MYSQL"
 #<> Database name
