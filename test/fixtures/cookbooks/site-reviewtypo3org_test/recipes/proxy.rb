335aba693073530439eb20b316b8543e8f625ef6
Steffen Gebert <steffen.gebert@typo3.org>
2017-12-02 12:00:47 +0100

    [TESTS] Queue notification to stop apache2

--- test/fixtures/cookbooks/site-reviewtypo3org_test/recipes/proxy.rb
+++ test/fixtures/cookbooks/site-reviewtypo3org_test/recipes/proxy.rb
@@ -1,5 +1,10 @@
 #node.default['nginx']['port'] = 8000
 resources('service[apache2]').action [:disable, :stop]
 
+log 'Notify service[apache2] to stop' do
+  notifies :stop, 'service[apache2]'
+  notifies :disable, 'service[apache2]'
+end
+
 node.default['site-proxytypo3org']['ssl_certificate'] = 'wildcard.vagrant'
 include_recipe 'site-proxytypo3org'
